d5fdfb541b2fea309c86d1841e000ea20e05d83c
unknown <jesseelijah.nance@gmail.com>
2022-12-05 18:27:25 -0500

    restructured event listener, created new id's for sections, created function to flow through quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,24 @@
+var home = document.querySelector('#home');
 var start = document.querySelector('#start');
-var nextQuestion = document.querySelector("#next");
+var question01 = document.querySelector("#qs01");
 var index = 0;
 
 start.addEventListener('click', function() {
-    question.setAttribute('style', 'display: block')
-    home.setAttribute('style', 'display: none') 
+    nextQuestion(index);
+    // console.log(me);
+    // question01.setAttribute('style', 'display: block')
+    // home.setAttribute('style', 'display: none') 
+
 });
+
+
+function nextQuestion(currIndex) {
+    //hide the current section and unhide the next section
+    var currPos = "qs0" + currIndex;
+    var nextPos = "qs0" + (currIndex + 1);
+
+    //Advance index in time for the next button click
+    index = index++;
+    console.log(currPos, nextPos);
+  
+}
