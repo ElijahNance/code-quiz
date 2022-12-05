18f34ab5277f2347159b87b5146d159626b11a43
unknown <jesseelijah.nance@gmail.com>
2022-12-05 16:44:23 -0500

    created event listener to move from home page to 1st question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
+var start = document.querySelector('#start');
+var nextQuestion = document.querySelector("#next");
+var index = 0;
 
+start.addEventListener('click', function() {
+    question.setAttribute('style', 'display: block')
+    home.setAttribute('style', 'display: none') 
+});
